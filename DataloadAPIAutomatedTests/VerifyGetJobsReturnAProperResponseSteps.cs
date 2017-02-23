94c603dfd26ee7beedd0cf911ceca1e9207c6fc9
LittyJohn <Litty.John@fourth.com>
2017-02-23 16:54:30 +0000

    New tests for API automation

---
+++ DataloadAPIAutomatedTests/VerifyGetJobsReturnAProperResponseSteps.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using TechTalk.SpecFlow;
+using RestSharp;
+using System.Configuration;
+using NUnit.Framework;
+using System.Net;
+
+
+namespace DataloadAPIAutomatedTests
+{
+    [Binding]
+    public class VerifyGetJobsReturnAProperResponseSteps
+    {
+        [Given(@"I can access the Dataload API as an authenticated user")]
+        public void GivenICanAccessTheDataloadAPIAsAnAuthenticatedUser()
+        {
+            var client = new RestClient(ConfigurationManager.AppSettings["RootUrl"]);
+            ScenarioContext.Current.Add("Client", client);
+
+
+        }
+        
+        [Then(@"I should get the response status is ok")]
+        public void ThenIShouldGetTheResponseStatusIsOk()
+        {
+            
+            Assert.AreEqual(HttpStatusCode.OK,ScenarioContext.Current.Get<RestResponse>("Response").StatusCode);
+        }
+
+
+        [When(@"I send a get request for the end point jobs ""(.*)""")]
+        public void WhenISendAGetRequestForTheEndPointJobs(string endpoint)
+        {
+            var request = new RestRequest(endpoint,Method.GET);
+            var client = ScenarioContext.Current.Get<RestClient>("Client");
+            var response = client.Execute(request);
+            ScenarioContext.Current.Add("Response", response);
+        }
+
+
+    }
+}
---
+++ DataloadAPIAutomatedTests/VerifyGetJobsReturnAResponse.feature
@@ -0,0 +1,15 @@
+﻿Feature: VerifyGetJobsReturnAProperResponse
+	In order to get a valid response
+	As a user
+	I want to request jobs api end point
+
+@mytag
+Scenario Outline: Get Processed Jobs from Jobs end point  
+	Given I can access the Dataload API as an authenticated user 
+	When I send a get request for the end point jobs "Dataload/Groups/<GroupID>/jobs"
+	Then I should get the response status is ok 
+
+	
+	Examples: 
+	| GroupID |
+	| 76 |
