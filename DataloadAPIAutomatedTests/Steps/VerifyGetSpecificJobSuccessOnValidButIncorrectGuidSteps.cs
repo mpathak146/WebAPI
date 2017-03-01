19bbfbbd9165fef9e403f9793de644f2d9616a75
Manish Pathak <manish.ptk@gmail.com>
2017-03-01 12:43:47 +0000

    Steps added

---
+++ DataloadAPIAutomatedTests/Steps/VerifyGetSpecificJobSuccessOnValidButIncorrectGuidSteps.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using TechTalk.SpecFlow;
+using RestSharp;
+using System.Configuration;
+using NUnit.Framework;
+using System.Net;
+
+
+namespace DataloadAPIAutomatedTests
+{
+    [Binding]
+    public partial class VerifyGetSpecificJobSuccessOnValidButIncorrectGuidSteps
+    {
+        [Then(@"I should get an empty response json from API")]
+        public void ThenIShouldGetAnEmptyResponseJsonFromAPI()
+        {
+            Assert.AreEqual(ScenarioContext.Current.Get<RestResponse>("Response").Content, "[]");
+
+        }
+
+
+
+    }
+}
