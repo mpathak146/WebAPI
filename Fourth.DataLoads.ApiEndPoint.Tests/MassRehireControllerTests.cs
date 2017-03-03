cc043516bcd6efd6a3721bae6d108f3edaae1b73
Manish Pathak <manish.ptk@gmail.com>
2017-03-03 12:16:48 +0000

    Controller Test added

---
+++ Fourth.DataLoads.ApiEndPoint.Tests/MassRehireControllerTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using Fourth.DataLoads.Data.Interfaces;
+using Fourth.DataLoads.ApiEndPoint.Authorization;
+using Fourth.DataLoads.ApiEndPoint.Controllers;
+
+namespace Fourth.PSLiveDataLoads.ApiEndPoint.Tests
+{
+    [TestClass]
+    public class MassRehireControllerTests
+    {
+        [TestMethod]
+        public void OnRequestRehireControllerIsNotNull()
+        {
+            var datafactory = new Mock<IDataFactory>(MockBehavior.Strict);
+            var authorization = new Mock<IAuthorizationProvider>(MockBehavior.Strict);
+            var controller = new MassRehireController(datafactory.Object, authorization.Object);
+            Assert.IsNotNull(controller);
+        }
+    }
+}
---
+++ Fourth.DataLoads.ApiEndPoint/Controllers/MassRehireController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Fourth.DataLoads.ApiEndPoint.Authorization;
+using Fourth.DataLoads.Data.Interfaces;
+
+namespace Fourth.DataLoads.ApiEndPoint.Controllers
+{
+    public class MassRehireController : ApiController
+    {
+        private IDataFactory dataFactory;
+        private IAuthorizationProvider authorizationProvider;
+
+        public MassRehireController(IDataFactory fact, 
+            IAuthorizationProvider auth)
+        {
+            this.dataFactory = fact;
+            this.authorizationProvider = auth;
+        }
+    }
+}
