be83cfb4bbdeeec917e98a9bb0ecf019494225b6
Manish Pathak <manish.ptk@gmail.com>
2017-03-07 16:39:07 +0000

    models

--- Fourth.DataLoads.Data/Models/MassRehireModel.cs
+++ Fourth.DataLoads.Data/Models/MassRehireModel.cs
@@ -7,12 +7,17 @@ namespace Fourth.DataLoads.Data.Entities
     public class MassRehireModel
     {
         public string EmployeeNumber { get; set; }
+        public string RehireDate { get; set; }
+
     }
     [Serializable]
     public class MassRehireModelSerialized : IMarker
     {
+        public Guid DataLoadJobId { get; set; }
+        public Guid DataLoadBatchId { get; set; }
         public string EmployeeNumber { get; set; }
-
+        public string RehireDate { get; set; }
+        public string ErrValidation { get; set; }
     }
 
 }
--- Fourth.DataLoads.Data/Models/MassTerminationModel.cs
+++ Fourth.DataLoads.Data/Models/MassTerminationModel.cs
@@ -6,12 +6,9 @@
     public partial class MassTerminationModel
     {
         
-        //public Guid DataLoadJobId { get; set; }
-        //public Guid DataLoadBatchId { get; set; }
         public string EmployeeNumber { get; set; }
         public string TerminationDate { get; set; }
         public string TerminationReason { get; set; }
-        //public string ErrValidation { get; set; }
 
     }
     [Serializable]
